2f25682c41edb03a8d54c5664887ed5f463d12c8
Esparrago23 <minisirhc@gmail.com>
2025-12-04 07:58:48 -0600

    Cors y datos BD

--- src/handlers/routes.go
+++ src/handlers/routes.go
@@ -4,16 +4,34 @@ import (
 	"api_concurrencia/src/moodle"
 	"api_concurrencia/src/repository"
 	"api_concurrencia/src/services"
+	"os"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"gorm.io/gorm"
 )
 
 func Routes(db *gorm.DB, moodleClient *moodle.Client) *chi.Mux {
 	r := chi.NewRouter()
 
+	// Configuración de CORS
+	allowedOrigins := []string{"https://*", "http://*"} // Valor por defecto
+	if envOrigins := os.Getenv("CORS_ALLOWED_ORIGINS"); envOrigins != "" {
+		allowedOrigins = strings.Split(envOrigins, ",")
+	}
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   allowedOrigins,
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
 	// Inicialización de Repositorios, Servicios y Handlers
-	
+
 	// --- PROGRAMA ESTUDIO (PE) ---
 	peRepo := repository.NewProgramaEstudioRepository(db)
 	peService := services.NewProgramaEstudioService(peRepo, moodleClient)
@@ -23,12 +41,12 @@ func Routes(db *gorm.DB, moodleClient *moodle.Client) *chi.Mux {
 	cRepo := repository.NewCuatrimestreRepository(db)
 	cService := services.NewCuatrimestreService(cRepo, moodleClient)
 	cHandler := NewCuatrimestreHandler(cService)
-	
+
 	// --- ASIGNATURA ---
 	aRepo := repository.NewAsignaturaRepository(db)
 	aService := services.NewAsignaturaService(aRepo, moodleClient)
 	aHandler := NewAsignaturaHandler(aService)
-	
+
 	// --- USUARIO ---
 	uRepo := repository.NewUsuarioRepository(db)
 	uService := services.NewUsuarioService(uRepo, moodleClient, aRepo)
@@ -42,18 +60,18 @@ func Routes(db *gorm.DB, moodleClient *moodle.Client) *chi.Mux {
 	r.Route("/programa-estudio", func(r chi.Router) {
 		r.Post("/", peHandler.CreateProgramaEstudio)
 		r.Get("/", peHandler.GetAllProgramaEstudio)
-		r.Post("/sync/{id}", peHandler.SyncProgramaEstudio) 
+		r.Post("/sync/{id}", peHandler.SyncProgramaEstudio)
 		r.Route("/{id}", func(r chi.Router) {
 			r.Get("/", peHandler.GetProgramaEstudioByID)
 			//r.Put("/", peHandler.UpdateProgramaEstudio)
 			//r.Delete("/", peHandler.DeleteProgramaEstudio)
 		})
 	})
-    
+
 	r.Route("/cuatrimestre", func(r chi.Router) {
 		r.Post("/", cHandler.CreateCuatrimestre)
 		r.Get("/", cHandler.GetAllCuatrimestres)
-		r.Post("/sync/{id}", cHandler.SyncCuatrimestre) 
+		r.Post("/sync/{id}", cHandler.SyncCuatrimestre)
 		r.Route("/{id}", func(r chi.Router) {
 			r.Get("/", cHandler.GetCuatrimestreByID)
 			r.Put("/", cHandler.UpdateCuatrimestre)
@@ -64,40 +82,37 @@ func Routes(db *gorm.DB, moodleClient *moodle.Client) *chi.Mux {
 	r.Route("/asignatura", func(r chi.Router) {
 		r.Post("/", aHandler.CreateAsignatura)
 		r.Get("/", aHandler.GetAllAsignaturas) // Asumiendo que implementaste GetAll
-		r.Post("/sync/{id}", aHandler.SyncAsignatura) 
+		r.Post("/sync/{id}", aHandler.SyncAsignatura)
 		r.Route("/{id}", func(r chi.Router) {
-			r.Get("/", aHandler.GetAsignaturaByID) // Asumiendo que implementaste GetByID
-			r.Put("/", aHandler.UpdateAsignatura)   // Asumiendo que implementaste Update
+			r.Get("/", aHandler.GetAsignaturaByID)   // Asumiendo que implementaste GetByID
+			r.Put("/", aHandler.UpdateAsignatura)    // Asumiendo que implementaste Update
 			r.Delete("/", aHandler.DeleteAsignatura) // Asumiendo que implementaste Delete
 		})
 	})
 
 	r.Route("/usuario", func(r chi.Router) {
 		r.Post("/", uHandler.CreateUsuario)
-		r.Get("/", uHandler.GetAllUsuarios) 
+		r.Get("/", uHandler.GetAllUsuarios)
 		r.Get("/unsynced", uHandler.GetUnsyncedUsuarios)
-		r.Get("/by_group/{grupoID}",uHandler.GetUsuariosByGroupID)
+		r.Get("/by_group/{grupoID}", uHandler.GetUsuariosByGroupID)
 		r.Post("/sync/{id}", uHandler.SyncUsuario)
 		r.Post("/enrol/{usuarioID}/{asignaturaID}", uHandler.MatricularUsuario)
 		r.Route("/{id}", func(r chi.Router) {
-			r.Get("/", uHandler.GetUsuarioByID) 
+			r.Get("/", uHandler.GetUsuarioByID)
 		})
 	})
 
-
 	r.Route("/grupo", func(r chi.Router) {
-		r.Post("/", gHandler.CreateGrupo) 
+		r.Post("/", gHandler.CreateGrupo)
 		r.Get("/", gHandler.GetAllGrupo)
-		r.Post("/sync/{id}",gHandler.SyncGrupo)
-		r.Post("/add-members/{grupoID}", gHandler.AddMembersToGroup) 
+		r.Post("/sync/{id}", gHandler.SyncGrupo)
+		r.Post("/add-members/{grupoID}", gHandler.AddMembersToGroup)
 		r.Route("/{id}", func(r chi.Router) {
-			r.Get("/", gHandler.GetGrupoByID) 
-			r.Put("/", gHandler.UpdateGrupo)   
+			r.Get("/", gHandler.GetGrupoByID)
+			r.Put("/", gHandler.UpdateGrupo)
 			r.Delete("/", gHandler.DeleteGrupo)
 		})
 	})
-
-
 
 	return r
 }
